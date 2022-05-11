04a1eb4ce3dbbb01416330d91bdf7f18588f5bfc
CamilleRungette <c.rungette@gmail.com>
2022-05-11 08:55:05 +0200

    loading shows ok

--- src/pages/Shows/Shows.js
+++ src/pages/Shows/Shows.js
@@ -53,73 +53,79 @@ const Shows = ({shows, deleteShowComp}) => {
 
   return (
     <div className='inside-app'>
+      {shows.length ? (
+        shows.map(show => (
+          <Accordion key={show._id} className='card show-main'>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <h2>{show.title} </h2>
+            </AccordionSummary>
+            <AccordionDetails>
+              <div className='show-buttons-div'>
+                <button className='btn' onClick={() => showModal(show)}>Modifier</button>
+                <Link to={`/spectacle/${show._id}/gallerie`}><button className='btn-outlined'>Voir la gallerie</button></Link>
+                <button className='btn-red-outlined' onClick={() => showDialog(show)}>Supprimer</button>
+              </div>
+              <p className='show-description' dangerouslySetInnerHTML={{__html: show.description}} />
+            { show.dates.length ? (
+                <ul className='no-list-style'>
+                  <h4> Dates </h4>
+                  {show.dates.map(date => (
+                    <li key={Math.floor(Math.random() * 1000000)} >
+                      <p>
+                        {date.place} {date.address ? <span>, {date.address},</span> : <></>} {date.city ? date.city + " " : <></>}
+                        |
+                        {new Date(date.startDate).getDate() === new Date(date.endDate).getDate() ?
+                          <span> Le {new Date(date.startDate).getDate()} {months[new Date(date.startDate).getMonth()]} </span>
+                        : 
+                        <span> Du {new Date(date.startDate).getDate()} 
+                            {new Date(date.startDate).getMonth() !== new Date(date.endDate).getMonth() ? (
+                            <span> {months[new Date(date.startDate).getMonth()]} </span>
+                            ):( <> </>)}
+                          au {new Date(date.endDate).getDate()} {months[new Date(date.endDate).getMonth()]}
+                        </span>}
+                      
+                      </p>
 
-      {shows.map(show => (
-        <Accordion key={show._id} className='card show-main'>
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1a-content"
-            id="panel1a-header"
-          >
-            <h2>{show.title} </h2>
-          </AccordionSummary>
-          <AccordionDetails>
-            <div className='show-buttons-div'>
-              <button className='btn' onClick={() => showModal(show)}>Modifier</button>
-              <Link to={`/spectacle/${show._id}/gallerie`}><button className='btn-outlined'>Voir la gallerie</button></Link>
-              <button className='btn-red-outlined' onClick={() => showDialog(show)}>Supprimer</button>
-            </div>
-            <p className='show-description' dangerouslySetInnerHTML={{__html: show.description}} />
-           { show.dates.length ? (
-              <ul className='no-list-style'>
-                <h4> Dates </h4>
-                {show.dates.map(date => (
-                  <li key={Math.floor(Math.random() * 1000000)} >
-                    <p>
-                      {date.place} {date.address ? <span>, {date.address},</span> : <></>} {date.city ? date.city + " " : <></>}
-                      |
-                      {new Date(date.startDate).getDate() === new Date(date.endDate).getDate() ?
-                        <span> Le {new Date(date.startDate).getDate()} {months[new Date(date.startDate).getMonth()]} </span>
-                      : 
-                      <span> Du {new Date(date.startDate).getDate()} 
-                          {new Date(date.startDate).getMonth() !== new Date(date.endDate).getMonth() ? (
-                          <span> {months[new Date(date.startDate).getMonth()]} </span>
-                          ):( <> </>)}
-                        au {new Date(date.endDate).getDate()} {months[new Date(date.endDate).getMonth()]}
-                      </span>}
-                    
-                    </p>
-
-                    <p ></p>
-                  </li>
-                ))}
-              </ul>) :(
-                <></>
-              )}
-
-            {show.links.length ? (
-              <ul className='no-list-style links-list'>
-                <h4>Liens</h4>
-                {show.links.map(link => (
-                  link.type === "pdf" ? (
-                    <li key={Math.floor(Math.random() * 1000000)}>
-                      <a href={link.link} target="_blank" rel="noopener noreferrer" className='show-link' >  {link.name} </a> <BsDownload className='icon'/>
+                      <p ></p>
                     </li>
-                  ) : (
-                    <li key={Math.floor(Math.random() * 1000000)}>
-                      <a href={link.link} target="_blank" rel="noopener noreferrer" className='show-link' >  {link.name} </a>
-                    </li>
-                  )
-                ))}
-              </ul> ) :( 
-                <></>
-              )}
-          </AccordionDetails>
-        </Accordion>
-      ))}
+                  ))}
+                </ul>) :(
+                  <></>
+                )}
+
+              {show.links.length ? (
+                <ul className='no-list-style links-list'>
+                  <h4>Liens</h4>
+                  {show.links.map(link => (
+                    link.type === "pdf" ? (
+                      <li key={Math.floor(Math.random() * 1000000)}>
+                        <a href={link.link} target="_blank" rel="noopener noreferrer" className='show-link' >  {link.name} </a> <BsDownload className='icon'/>
+                      </li>
+                    ) : (
+                      <li key={Math.floor(Math.random() * 1000000)}>
+                        <a href={link.link} target="_blank" rel="noopener noreferrer" className='show-link' >  {link.name} </a>
+                      </li>
+                    )
+                  ))}
+                </ul> ) :( 
+                  <></>
+                )}
+            </AccordionDetails>
+          </Accordion>
+        ))
+      ) : ( 
+        <div className='loading-div'>
+          <img src="/images/loading.gif" alt="events-loader" />
+        </div>
+      )}
     <BasicModal ref={modalRef} content={<EditShow showAlert={showAlert} showInfos={show} closeModal={closeModal} />} />
     <Alert ref={alertRef} type={alert.type} message={alert.message} />
-    <ConfirmModal ref={confirmRef} content={<div>Êtes-vous sûr de vouloir supprimer ce spectacle ?</div> } button={true} confirmParent={deleteShow}  />    </div>
+    <ConfirmModal ref={confirmRef} content={<div>Êtes-vous sûr de vouloir supprimer ce spectacle ?</div> } button={true} confirmParent={deleteShow}  />    
+    </div>
   )
 }
 
